2fe5e0f71fe7a945a38902692c1c42f2755ab7ec
pasha <askerovpasha21@gmail.com>
2024-03-02 20:07:50 +0400

    test metadata

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'swiper/css';
@@ -10,6 +10,8 @@ import store from '../redux/store';
 import { Provider } from 'react-redux';
 import { LocaleType } from '../types/general/type';
 import { Fancybox } from '@fancyapps/ui';
+import { SiteSettingDataType, SiteSettingTranslateDataType } from '../types/data/type';
+import { Settings } from '../class';
 
 
 
@@ -21,8 +23,49 @@ type LayoutProps = {
 
 const RootLayout: React.FC<LayoutProps> = ({ activeLocale, children, dictionary }) => {
     useEffect(() => { Fancybox.bind("[data-fancybox]", {}) }, []);
+
+    const setting = new Settings();
+    const [settingData, setSettingData] = useState<SiteSettingDataType>();
+    const [settingTranslateData, setSettingTranslateData] = useState<SiteSettingTranslateDataType[]>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response: {
+                main: SiteSettingDataType,
+                translate: SiteSettingTranslateDataType[]
+            } = await setting.active(1);
+            if (response.main && response.translate) {
+                setSettingData(response.main);
+                setSettingTranslateData(response.translate)
+            }
+        }
+        fetchData();
+    }, [])
+
     return (
         <Provider store={store}>
+            {
+                settingData && settingTranslateData && (
+                    <div>
+                        <div>
+                            {setting.getTranslate({
+                                id: 1,
+                                activeLocale,
+                                key: 'title',
+                                translateData: settingTranslateData
+                            })}
+                        </div>
+                        <div>
+                            {setting.getTranslate({
+                                id: 1,
+                                activeLocale,
+                                key: 'description',
+                                translateData: settingTranslateData
+                            })}
+                        </div>
+                    </div>
+                )
+            }
             <main>{children}</main>
         </Provider>
     )
