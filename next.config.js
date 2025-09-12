077e414839bd5f76cbe2b6541cafd1d9f52a2639
pasha <askerovpasha21@gmail.com>
2025-09-12 20:19:07 +0400

    update config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ['127.0.0.1','geoprobackend.proton.az','back.geoenergypro.com','geoenergypro.com'],
+        domains: ['127.0.0.1','geoprobackend.proton.az','back.geoenergypro.com','geoenergypro.com','geopro-backend-production.up.railway.app'],
     },
     env: {
         BASE_URL: process.env.BASE_URL
